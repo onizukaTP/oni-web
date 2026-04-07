55d112665b0e83f1b996e9ede14a8fed4d042881
onizukaTP <tharunprabu2418@gmail.com>
2026-04-07 18:36:42 +0530

    feat: added default response by using constructor overloading

--- src/main/java/com/onizuka/framework/http/HttpResponse.java
+++ src/main/java/com/onizuka/framework/http/HttpResponse.java
@@ -1,11 +1,24 @@
 package com.onizuka.framework.http;
 
-public class Response {
+import java.util.Map;
+
+public class HttpResponse {
     public int status;
+    public Map<String, String> header;
     public String body;
 
-    public Response(int status, String body) {
+    public HttpResponse(int status, String body) {
+        // Map.of() immutable
+        this(status, Map.of("Content-Type", "text/plain; charset=UTF-8"), body);
+    }
+
+    public HttpResponse(int status, Map<String, String> header, String body) {
         this.status = status;
+        this.header = header;
         this.body = body;
+    }
+
+    public void addHeader(String key, String value) {
+        header.put(key, value);
     }
 }
