73d19072b816c0cf74ef4d43bd98c2a96e4ba2fa
onizukaTP <tharunprabu2418@gmail.com>
2026-04-07 18:38:11 +0530

    feat: added a handler and toString method

--- src/main/java/com/onizuka/framework/server/routing/Route.java
+++ src/main/java/com/onizuka/framework/server/routing/Route.java
@@ -1,17 +1,26 @@
 package com.onizuka.framework.server.routing;
 
 import com.onizuka.framework.http.HttpRequest;
+import com.onizuka.framework.http.HttpResponse;
 
 import java.util.function.Function;
 
 public class Route {
     public String method;
     public String pathPattern;
-    public Function<HttpRequest, String> handler;
+    public Function<HttpRequest, HttpResponse> handler;
 
-    public Route(String method, String pathPattern, Function<HttpRequest, String> handler) {
+    public Route(String method, String pathPattern, Function<HttpRequest, HttpResponse> handler) {
         this.method = method;
         this.pathPattern = pathPattern;
         this.handler = handler;
+    }
+
+    @Override
+    public String toString() {
+        return "Route{" +
+                "method='" + method + '\'' +
+                ", pathPattern='" + pathPattern + '\'' +
+                '}';
     }
 }
