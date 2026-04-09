7304fd6a01b9be9aa72240ca816325018fc0916f
onizukaTP <tharunprabu2418@gmail.com>
2026-04-09 11:41:26 +0530

    refactor: renamed attribute

--- src/main/java/com/onizuka/framework/http/HttpResponse.java
+++ src/main/java/com/onizuka/framework/http/HttpResponse.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class HttpResponse {
     public int status;
-    public Map<String, String> header;
+    public Map<String, String> headers;
     public String body;
 
     public HttpResponse(int status, String body) {
@@ -12,13 +12,13 @@ public class HttpResponse {
         this(status, Map.of("Content-Type", "text/plain; charset=UTF-8"), body);
     }
 
-    public HttpResponse(int status, Map<String, String> header, String body) {
+    public HttpResponse(int status, Map<String, String> headers, String body) {
         this.status = status;
-        this.header = header;
+        this.headers = headers;
         this.body = body;
     }
 
     public void addHeader(String key, String value) {
-        header.put(key, value);
+        headers.put(key, value);
     }
 }
