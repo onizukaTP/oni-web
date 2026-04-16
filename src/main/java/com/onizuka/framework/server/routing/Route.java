65eef3a944bdf81ba5f3e34c17d79d6f87d9db20
onizukaTP <tharunprabu2418@gmail.com>
2026-04-16 12:47:16 +0530

    feat: update handler using helper function

--- src/main/java/com/onizuka/framework/server/routing/Route.java
+++ src/main/java/com/onizuka/framework/server/routing/Route.java
@@ -1,19 +1,21 @@
 package com.onizuka.framework.server.routing;
 
 import com.onizuka.framework.http.HttpRequest;
-import com.onizuka.framework.http.HttpResponse;
-
-import java.util.function.Function;
+import com.onizuka.framework.util.Handler;
 
 public class Route {
     public String method;
     public String pathPattern;
-    public Function<HttpRequest, HttpResponse> handler;
+    public Handler handler;
 
-    public Route(String method, String pathPattern, Function<HttpRequest, HttpResponse> handler) {
+    public Route(String method, String pathPattern, Handler handler) {
         this.method = method;
         this.pathPattern = pathPattern;
         this.handler = handler;
+    }
+
+    public Object handle(HttpRequest req) {
+        return handler.handle(req);
     }
 
     @Override
