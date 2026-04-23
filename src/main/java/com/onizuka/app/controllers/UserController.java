4425f7a8be01626aca44bb5351a447d5b4ae58aa
onizukaTP <tharunprabu2418@gmail.com>
2026-04-23 18:40:44 +0530

    feat: test annotations

--- src/main/java/com/onizuka/app/controllers/UserController.java
+++ src/main/java/com/onizuka/app/controllers/UserController.java
@@ -2,18 +2,25 @@ package com.onizuka.app.controllers;
 
 import com.onizuka.framework.annotations.GET;
 import com.onizuka.framework.annotations.PathParam;
+import com.onizuka.framework.annotations.QueryParam;
 import com.onizuka.framework.http.HttpRequest;
 import com.onizuka.framework.http.HttpResponse;
 
 public class UserController {
 
-    @GET("/users")
-    public HttpResponse getUsers(HttpRequest req) {
-        return new HttpResponse(200, "All users"); // hard corded response
-    }
-
     @GET("/users/{id}")
     public HttpResponse getUser(@PathParam("id") String id) {
         return new HttpResponse(200, "user " + id);
+    }
+
+    @GET("/users")
+    public HttpResponse getUsers(
+            HttpRequest req,
+            @QueryParam("page") String page
+    ) {
+        if (page != null) {
+            return new HttpResponse(200, "Page: " + page);
+        }
+        return new HttpResponse(200, "All users");
     }
 }
