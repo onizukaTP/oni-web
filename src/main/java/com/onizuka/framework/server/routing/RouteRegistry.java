0a87906c6a477715b021c5e19d023651e5bea30c
onizukaTP <tharunprabu2418@gmail.com>
2026-04-23 18:39:55 +0530

    feat: update route registry to scan query params and handle it

--- src/main/java/com/onizuka/framework/server/routing/RouteRegistry.java
+++ src/main/java/com/onizuka/framework/server/routing/RouteRegistry.java
@@ -2,10 +2,10 @@ package com.onizuka.framework.server.routing;
 
 import com.onizuka.framework.annotations.GET;
 import com.onizuka.framework.annotations.PathParam;
+import com.onizuka.framework.annotations.QueryParam;
 import com.onizuka.framework.http.HttpRequest;
 import com.onizuka.framework.http.HttpResponse;
 import com.onizuka.framework.util.Handler;
-
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
@@ -14,59 +14,111 @@ import java.util.List;
 
 public class RouteRegistry {
 
+    // Stores all registered routes
     private static final List<Route> routes = new ArrayList<>();
 
-    public static void registerRoutes(Class<?> controllerClass) { // can also use clazz or type
+    /**
+     * This method scans a controller class using reflection
+     * and converts annotated methods into executable routes.
+     */
+    public static void registerRoutes(Class<?> controllerClass) {
         try {
+            // Create ONE instance of the controller (singleton-style)
             Object controllerInstance = controllerClass
                     .getDeclaredConstructor()
                     .newInstance();
 
+            // Loop through all methods in the controller
             for (Method method : controllerClass.getDeclaredMethods()) {
 
-                // Detect @GET
-                if (method.isAnnotationPresent(GET.class)) {
+                // Only process methods annotated with @GET
+                if (!method.isAnnotationPresent(GET.class)) continue;
 
-                    // temp simple validation
-                    if (method.getParameterCount() != 1 ||
-                            method.getParameterTypes()[0] != HttpRequest.class) {
-                        throw new RuntimeException("Invalid handler method: " + method.getName());
-                    }
-                    GET get = method.getAnnotation(GET.class);
-                    String path = get.value();
+                GET get = method.getAnnotation(GET.class);
+                String path = get.value();
 
-                    // Create handler
-                    Handler handler = (HttpRequest req) -> {
-                        try {
-                            Parameter[] parameters = method.getParameters();
-                            Object[] args = new Object[parameters.length];
+                // Allow private methods to be invoked
+                method.setAccessible(true);
 
-                            for (int i = 0; i < parameters.length; i++) {
-                                Parameter param = parameters[i];
+                /**
+                 * Validate method parameters
+                 * Supported:
+                 *  - HttpRequest
+                 *  - @PathParam
+                 *  - @QueryParam
+                 */
+                for (Parameter param : method.getParameters()) {
 
-                                if (param.isAnnotationPresent(PathParam.class)) {
+                    if (param.getType() == HttpRequest.class) continue;
 
-                                    String name = param.getAnnotation(PathParam.class).value();
+                    if (param.isAnnotationPresent(PathParam.class)) continue;
 
-                                    String value = req.getPathParam(name);
+                    if (param.isAnnotationPresent(QueryParam.class)) continue;
 
-                                    args[i] = value; // for now assume String only
-                                }
+                    throw new RuntimeException(
+                            "Unsupported parameter in " + method.getName() +
+                                    ": " + param.getName()
+                    );
+                }
+
+                /**
+                 * Create a handler (lambda)
+                 * This is the KEY part:
+                 * We pre-build execution logic so dispatcher stays simple.
+                 */
+                Handler handler = (HttpRequest req) -> {
+                    try {
+                        Parameter[] parameters = method.getParameters();
+                        Object[] args = new Object[parameters.length];
+
+                        // Resolve arguments dynamically at runtime
+                        for (int i = 0; i < parameters.length; i++) {
+                            Parameter param = parameters[i];
+
+                            // Inject full request
+                            if (param.getType() == HttpRequest.class) {
+                                args[i] = req;
                             }
 
-                            method.setAccessible(true);
-                            return (HttpResponse) method.invoke(controllerInstance, req);
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
+                            // Inject path variable
+                            else if (param.isAnnotationPresent(PathParam.class)) {
+                                String name = param.getAnnotation(PathParam.class).value();
+                                String value = req.getPathParam(name);
+
+                                if (value == null) {
+                                    throw new RuntimeException("Missing path param: " + name);
+                                }
+
+                                args[i] = value;
+                            }
+
+                            // Inject query param
+                            else if (param.isAnnotationPresent(QueryParam.class)) {
+                                String name = param.getAnnotation(QueryParam.class).value();
+                                args[i] = req.getQueryParam(name); // can be null
+                            }
+
+                            else {
+                                throw new RuntimeException(
+                                        "Unsupported parameter: " + param.getName()
+                                );
+                            }
                         }
-                    };
 
-                    // Register route
-                    Route route = new Route("GET", path, handler);
-                    routes.add(route);
+                        // Invoke controller method via reflection
+                        return (HttpResponse) method.invoke(controllerInstance, args);
 
-                    System.out.println("Registered GET " + path);
-                }
+                    } catch (Exception e) {
+                        System.out.println(e.toString());
+                        return new HttpResponse(500, "Internal Server Error");
+                    }
+                };
+
+                // Create and store route
+                Route route = new Route("GET", path, handler);
+                routes.add(route);
+
+                System.out.println("Registered GET " + path);
             }
 
         } catch (Exception e) {
@@ -74,5 +126,11 @@ public class RouteRegistry {
         }
     }
 
-    public static List<Route> getRoutes() {return routes;}
+    /**
+     * Returns all registered routes
+     * Dispatcher will use this
+     */
+    public static List<Route> getRoutes() {
+        return routes;
+    }
 }
